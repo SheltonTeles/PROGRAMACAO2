1e8067a892a1bba26af2910d3f1ad866461a028a
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2024-01-14 00:18:06 +0200

    Primeiro exercício POO

---
+++ src/aluno1/Enunciado.java
@@ -0,0 +1,11 @@
+package aluno1;
+
+public class Enunciado {
+
+	/*
+	 * Crie uma classe "Aluno" com os atributos "nome", "idade", "matricula" e
+	 * "nota". Adicione métodos para calcular a média do aluno e para verificar se
+	 * ele foi aprovado (média 7). Implemente encapsulamento de modo que os
+	 * atributos sé possam ser acedidos e modificados dentro da pr6pria classes
+	 */
+}
