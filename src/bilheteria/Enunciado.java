c5607efaa1115e488b9e76885be8f4045cd0e62a
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2024-01-14 15:40:42 +0200

    Bilheteria exercício

---
+++ src/bilheteria/Enunciado.java
@@ -0,0 +1,16 @@
+package bilheteria;
+
+public class Enunciado {
+
+	/*
+	 * Crie uma classe chamada Bilhete que possui tun valor em meticais e um método
+	 * unpruneValor(). a. Crie tuna classe VIP, que herda Bilhete e possui um valor
+	 * adicional. Crie tun método que retome o valor do Bilhete VIP .com o adicional
+	 * incluido) b. Crie tuna classe Nonnal, que herda Bilbete e possui um método
+	 * que imprime: • Bilhete Nonnar C. Crie tuna classe Camarotelnferior (que
+	 * possui a localizagåo do Bilhete e métodos pan acessar e unprunir esta
+	 * localtzagåo) e uma classe CamaroteSuperior, que é mars cara (possui valor
+	 * adicional). Esta ültima possui um método para retomar o valor do Bilhete.
+	 * Ambas as classes herdam a classe VIP.
+	 */
+}
