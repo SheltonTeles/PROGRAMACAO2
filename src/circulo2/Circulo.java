88f73a45734e2d6149e354c650bd3320f05be7ad
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2024-01-14 12:41:23 +0200

    Primeiro exercício POO

---
+++ src/circulo2/Circulo.java
@@ -0,0 +1,31 @@
+package circulo2;
+
+public class Circulo {
+	private double raio;
+
+	public double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+
+	public Circulo(double raio) {
+		super();
+		this.raio = raio;
+	}
+
+	public Circulo() {
+	}
+
+	public double calcularArea(double raio) {
+
+		return 0;
+	}
+
+	public double calcularPerimetro(double raio) {
+
+		return 0;
+	}
+}
---
+++ src/circulo2/Enunciado.java
@@ -0,0 +1,11 @@
+package circulo2;
+
+public class Enunciado {
+
+	/*
+	 * 2. Crie uma classe "Circulo" com o atributo "rato" e os métodos e "
+	 * Implemente encapsulamento de modo que o raio Sö possa ser modificado através
+	 * de um método especifico, que verifique se o valor passado é maior que zero.
+	 * Adicione um atributo "cor" somente leitura å classe.
+	 */
+}
