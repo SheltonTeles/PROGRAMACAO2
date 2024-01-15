a9f8a8c0edc9fcffe0e391567e3119c35966105f
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2024-01-15 08:31:30 +0200

    Exercício Círculo

--- src/circulo2/Enunciado.java
+++ src/circulo2/Enunciado.java
@@ -3,7 +3,7 @@ package circulo2;
 public class Enunciado {
 
 	/*
-	 * 2. Crie uma classe "Circulo" com o atributo "raio" e os métodos e "
+	 * 2. Crie uma classe "Círculo" com o atributo "raio" e os métodos e "
 	 * Implemente encapsulamento de modo que o raio Sö possa ser modificado através
 	 * de um método especifico, que verifique se o valor passado é maior que zero.
 	 * Adicione um atributo "cor" somente leitura à classe.
